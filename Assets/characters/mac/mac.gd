91f604c28971b8d93479115e493da14cd8e81322
Emily Skelley <eskelley05@gmail.com>
2025-01-11 18:05:23 -0800

    Created levelmanager (NOT FUNCTIONAL)
    A progress save for the level manager

--- Assets/characters/mac/mac.gd
+++ Assets/characters/mac/mac.gd
@@ -4,8 +4,11 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var can_control : bool = true
 
 func _physics_process(delta: float) -> void:
+	if not can_control: return
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -13,7 +16,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		print(get_tree().current_scene)
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
@@ -23,3 +26,14 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func handle_danger() -> void:
+	print("mac died")
+	visible = false
+	can_control = false
+	
+	await get_tree().create_timer(1).timeout
+	reset_mac()
+	
+func reset_mac() -> void:
+	global_position = LevelManager.loaded_level.level_start_position.global_position
---
+++ scenes/danger_zone.gd
@@ -0,0 +1,3 @@
+extends Area2D
+
+# signal for body entering this area
