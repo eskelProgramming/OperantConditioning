744e4dbc2b35ed0e8eaa032d220a7a4f0024f881
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2025-01-13 09:58:07 -0800

    Level Manager now fully functional
    The Level Manager can load and unload scenes.

--- scripts/managers/level_manager.gd
+++ scripts/managers/level_manager.gd
@@ -13,6 +13,7 @@ func unload_level() -> void:
 	
 func load_level(level_id : int) -> void:
 	print("Loading level: %s" % level_id)
+	print(levels.size())
 	unload_level()
 	
 	var level_data = get_level_data_by_id(level_id)
@@ -20,8 +21,8 @@ func load_level(level_id : int) -> void:
 	if not level_data:
 		return
 		
-	var level_path = "res://scenes/%s/tscn" % level_data.level_path
-	print(level_path)
+	var level_path = "res://scenes/%s.tscn" % level_data.level_path
+	print("Has level path: %s" % level_path)
 	var level_res := load(level_path)
 	
 	if level_res:
@@ -33,9 +34,12 @@ func load_level(level_id : int) -> void:
 	
 func get_level_data_by_id(id : int) -> LevelData:
 	var level_to_return : LevelData = null
-	
+	print("id in get_level_data_by_id %s" % id)
+	print(levels.size())	
 	for lvl : LevelData in levels:
+		print("level id: %s" % lvl.level_id)
 		if lvl.level_id == id:
 			level_to_return = lvl
+			print(level_to_return)
 			
 	return level_to_return
