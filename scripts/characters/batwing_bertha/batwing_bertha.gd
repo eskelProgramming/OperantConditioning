2319efc7460f3730fa0f528e80ad0d48b82c7b82
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2025-01-14 16:24:22 -0800

    Batwing Bertha Movement functional
    Batwing Bertha can now move between two Node2ds.

---
+++ scripts/characters/batwing_bertha/batwing_bertha.gd
@@ -0,0 +1,29 @@
+class_name BatwingBertha
+extends CharacterBody2D
+
+@export var start_pos : Node2D
+@export var end_pos : Node2D
+@export var move_speed : float
+
+var target_positions = []
+var current_target = 0
+
+func _ready() -> void:
+	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
+
+	# Ensure the Node2D nodes are assigned
+	if start_pos and end_pos:
+		target_positions.append(start_pos.global_position)
+		target_positions.append(end_pos.global_position)
+	else:
+		print("Error: Area2D nodes are not assigned properly.")
+		return
+
+func _physics_process(delta):
+	if target_positions.size() > 0:
+		var direction = (target_positions[current_target] - global_position).normalized()
+		global_position += direction * move_speed * delta
+
+		# Check if the character has reached the target position
+		if global_position.distance_to(target_positions[current_target]) < 5:
+			current_target = (current_target + 1) % target_positions.size()
