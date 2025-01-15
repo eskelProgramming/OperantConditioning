1201ce9058d9d05911a19299f96a008fcd418a3c
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2025-01-15 11:41:15 -0800

    Magma Jimmy Movement fully functional

--- scripts/characters/magma_jimmy.gd
+++ scripts/characters/magma_jimmy.gd
@@ -1,32 +1,27 @@
+class_name MagmaJimmy
 extends CharacterBody2D
 
 @export var mac : Mac
 @export var speed : float
-@export var navigation_agent : NavigationAgent2D
-@export var navigation_region : NavigationRegion2D
-@export var ray_left : RayCast2D
-@export var ray_right : RayCast2D
+@export var left_boundary : Area2D
+@export var right_boundary : Area2D
+
+var can_move_left = true
+var can_move_right = true
 
 func _ready():
-	navigation_agent.set_navigation_map(navigation_region)
+	pass
 
 func _physics_process(delta):
 	if mac:
-		navigation_agent.set_target_position(mac.global_position)
-		if not navigation_agent.is_navigation_finished():
-			var direction = (navigation_agent.get_next_path_position() - global_position).normalized()
-			velocity = direction * speed
-			print("Velocity: ", velocity)
-			check_edges()
-		move_and_slide()
+		var direction = (mac.position - position).normalized()
+		# Adjust movement based on area collision detection
+		if direction.x < 0 and not can_move_left:
+			velocity.x = 0
+		elif direction.x > 0 and not can_move_right:
+			velocity.x = 0
+		else:
+			velocity.x = direction.x * speed
+		velocity.y = 60
 
-func check_edges():
-	ray_left.force_raycast_update()
-	ray_right.force_raycast_update()
-	print("Ray Left Colliding: ", ray_left.is_colliding())
-	print("Ray Right Colliding: ", ray_right.is_colliding())
-	if ray_left.is_colliding() or ray_right.is_colliding():
-		velocity = Vector2.ZERO  # Stop moving if an edge is detected
-		print("Edge detected, stopping movement")
-	else:
-		print("No edge detected, continuing movement")
+		move_and_slide()
