0733e9aab1b803720f88f7135a3fb8309bb33f68
Emily Skelley <eskelley05@gmail.com>
2025-01-14 21:50:38 -0800

    Progress Save

--- scripts/characters/magma_jimmy.gd
+++ scripts/characters/magma_jimmy.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 @export var speed : float
 @export var navigation_agent : NavigationAgent2D
 @export var navigation_region : NavigationRegion2D
+@export var ray_left : RayCast2D
+@export var ray_right : RayCast2D
 
 func _ready():
 	navigation_agent.set_navigation_map(navigation_region)
@@ -14,13 +16,17 @@ func _physics_process(delta):
 		if not navigation_agent.is_navigation_finished():
 			var direction = (navigation_agent.get_next_path_position() - global_position).normalized()
 			velocity = direction * speed
+			print("Velocity: ", velocity)
 			check_edges()
-			move_and_slide()
+		move_and_slide()
 
 func check_edges():
-	var ray_left = $RayCast2D_Left
-	var ray_right = $RayCast2D_Right
 	ray_left.force_raycast_update()
 	ray_right.force_raycast_update()
-	if not ray_left.is_colliding() or not ray_right.is_colliding():
+	print("Ray Left Colliding: ", ray_left.is_colliding())
+	print("Ray Right Colliding: ", ray_right.is_colliding())
+	if ray_left.is_colliding() or ray_right.is_colliding():
 		velocity = Vector2.ZERO  # Stop moving if an edge is detected
+		print("Edge detected, stopping movement")
+	else:
+		print("No edge detected, continuing movement")
