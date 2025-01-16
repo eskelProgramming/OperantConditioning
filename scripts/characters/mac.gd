b61fec53171a0d67dabce398c1036ba6f11c539e
Emily Skelley <eskelley05@gmail.com>
2025-01-15 20:33:23 -0800

    Progress save

--- scripts/characters/mac.gd
+++ scripts/characters/mac.gd
@@ -1,11 +1,13 @@
 class_name Mac
 extends CharacterBody2D
 
+@export var throw_speed = 100.0
 
 const SPEED = 250.0
 const JUMP_VELOCITY = -350.0
 
 var can_control : bool = true
+var is_slowing_time = false
 
 func _physics_process(delta: float) -> void:
 	if not can_control: return
@@ -14,10 +16,19 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	if Input.is_action_just_pressed("slow_time"):
+		is_slowing_time = true
+		Engine.time_scale = 0.5  # Slow down time
+	elif Input.is_action_just_released("slow_time"):
+		is_slowing_time = false
+		Engine.time_scale = 1.0  # Restore normal time
+
+	if Input.is_action_just_pressed("throw"):
+		throw_object()
+
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		print(get_tree().current_scene)
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
@@ -40,3 +51,10 @@ func reset_mac() -> void:
 	global_position = LevelManager.loaded_level.level_start_position.global_position
 	visible = true
 	can_control = true
+
+func throw_object():
+	var throw_instance = preload("res://scenes/assets/wrench.tscn").instantiate()
+	get_parent().add_child(throw_instance)
+	throw_instance.global_position = global_position
+	var direction = (get_global_mouse_position() - global_position).normalized()
+	throw_instance.linear_velocity = direction * throw_speed
