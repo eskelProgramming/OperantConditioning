65ce175827a35798c44706d44c8075240ab25a5a
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2025-01-16 12:28:21 -0800

    Progress save

---
+++ scripts/managers/coin_manager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var coins : int = 0
+
+func add_coins(num_coins_to_add : int) :
+	coins += num_coins_to_add
+	if coins > 100:
+		var lives_to_add = (coins / 100) % 10
+		remove_coins(lives_to_add * 100)
+		LifeManager.add_lives(lives_to_add)
+	
+func remove_coins(num_coins_to_remove : int) :
+	coins -= num_coins_to_remove
