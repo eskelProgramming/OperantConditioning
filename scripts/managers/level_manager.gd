ffe2b48cb8b34c3048ef22d0e72f18af1e2ed0a2
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2025-01-17 13:47:54 -0800

    Removed Print statements
    Removed extra print statements

--- scripts/managers/level_manager.gd
+++ scripts/managers/level_manager.gd
@@ -12,31 +12,25 @@ func unload_level() -> void:
 	loaded_level = null
 	
 func load_level(level_id : int) -> void:
-	print("Loading level: %s" % level_id)
 	unload_level()
 	
 	var level_data = get_level_data_by_id(level_id)
 	
-	
 	if not level_data:
-		print("level_data is null")
 		return
 		
 	var level_path = "res://scenes/levels/%s.tscn" % level_data.level_path
 	var level_res := load(level_path)
 	
 	if level_res:
-		print(loaded_level)
 		loaded_level = level_res.instantiate()
 		
 		main_scene.add_child(loaded_level)
-		print(loaded_level.level_data.level_name)
 	else:
 		print("level does not exist")
 	
 func get_level_data_by_id(id : int) -> LevelData:
 	var level_to_return : LevelData = null
-	print(levels.size())	
 	for lvl : LevelData in levels:
 		if lvl.level_id == id:
 			level_to_return = lvl
