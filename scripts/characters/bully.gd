06e3bfd58b293c62d5759e896813437964d68ca6
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2025-02-10 10:23:22 -0800

    Fixed Bully
    Bully now does not need to have a "mac" object directly added to it

--- scripts/characters/bully.gd
+++ scripts/characters/bully.gd
@@ -6,6 +6,8 @@ extends CharacterBody2D
 var hit_count = 0
 
 func _physics_process(delta: float) -> void:
+	check_and_get_mac()
+		
 	if mac.global_position.distance_to(global_position) < 400:
 		rush()
 	else:
@@ -25,3 +27,7 @@ func add_hits(num_hits_to_add: int):
 func check_hit_count():
 	if hit_count >= 3:
 		get_tree().queue_delete(self)
+
+func check_and_get_mac() :
+	if (mac == null) :
+		mac = get_tree().get_nodes_in_group("mac")[0]
