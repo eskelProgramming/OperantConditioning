b4d282ac185e8372dae377c2b52d2b958b40a7ce
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2025-03-07 10:15:17 -0800

    Coin documentation finished

--- scripts/assets/coin.gd
+++ scripts/assets/coin.gd
@@ -1,25 +1,22 @@
-## coin.gd
-## This script defines a Coin class, which represents a collectible coin in the game.
-## Coins can be collected by a specific type of object (e.g., a character of type Mac).
-
-## Declare this script as a Coin class
+## Represents a collectible coin in the game.
+##
+## Coins can be collected by either [Mac] or a [Wrench].
+## Coins are tracked by the [CoinManager].
 class_name Coin
 
-## Extends Area2D, allowing the coin to detect overlapping objects
+# Inherits from [Area2D]
 extends Area2D
 
-## Determines whether the coin can currently be collected
+## Determines whether the coin can currently be collected.
 @export var can_be_collected: bool = true
 
-## Callback triggered when another body enters the coin's area
-## Parameters:
-##  body (Node2D): The body that entered the area
+## Checks if [param body] is [Mac]. If it is, [method collect_coin]
+## is called. 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Mac:
 		collect_coin()
 
-## Handles the logic for collecting the coin.
-## Adds one coin to the CoinManager and marks the coin as collected
+## Adds one coin to the CoinManager and marks the coin as collected.
 func collect_coin():
 	if can_be_collected:
 		CoinManager.add_coins(1)
