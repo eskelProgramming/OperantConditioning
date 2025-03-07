299f1c0f1eb5e56e2c4e668b291401e8f7f4d69e
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2025-03-07 11:32:22 -0800

    DangerZone documentation finished

--- scripts/classes/danger_zone.gd
+++ scripts/classes/danger_zone.gd
@@ -1,8 +1,9 @@
+## An area that will kill [Mac] on collision.
+class_name DangerZone
+
 extends Area2D
 
-# signal for body entering this area
-
-
+## If [Mac] is [param body], [method Mac.handle_danger].
 func _on_body_entered(body: Node2D) -> void:
 	if body is Mac:
 		body.handle_danger()
