b7143d27f335ccbcb25fbcaac1d934a008619fd7
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2025-03-06 19:30:29 -0800

    Prog Save

--- scripts/characters/batwing_bertha.gd
+++ scripts/characters/batwing_bertha.gd
@@ -1,33 +1,34 @@
-## batwing_bertha.gd
-## This script defines the BatwingBertha class, which represents a floating character 
-## that alternates between two target positions in the game world.
-
-## Declare this script as a BatwingBertha class
+## A floating character that alternates between two target positions in the game world.
+##
+## This class inherits from [CharacterBody2D], and is an enemy 
+## that can interact with [Mac]. If [Mac] collides with a 
+## [BatwingBertha], [method Mac.reset_mac] is called and 
+## the level is reset.  
 class_name BatwingBertha
 
 ## Extends CharacterBody2D to utilize character physics and motion capabilities
 extends CharacterBody2D
 
-## Exported properties
-## start_pos: Node2D - The starting position for the character
+## The starting position for the character
 @export var start_pos: Node2D
-## end_pos: Node2D - The ending position for the character
+## The ending position for the character
 @export var end_pos: Node2D
-## move_speed: float - The speed at which the character moves between positions
-@export var move_speed: float
+## The speed at which the character moves between positions
+@export var move_speed: float = 20.0
 
 ## Internal variables
-## target_positions: Array - Stores the global positions of the start and end nodes
-var target_positions = []
-## current_target: int - Tracks the current target position index
+
+## Stores the global positions of the start and end nodes
+var target_positions: = [Node2D]
+## Tracks the current target position index
 var current_target = 0
 
 ## Called when the node enters the scene tree
 func _ready() -> void:
-	## Set the motion mode to floating
+	# Set the motion mode to floating
 	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
 
-	## Ensure start_pos and end_pos nodes are assigned
+	# Ensure start_pos and end_pos nodes are assigned
 	if start_pos and end_pos:
 		target_positions.append(start_pos.global_position)
 		target_positions.append(end_pos.global_position)
@@ -35,10 +36,9 @@ func _ready() -> void:
 		print("Error: Node2D nodes are not assigned properly.")
 		return
 
-## Called every physics frame to handle character movement
-## Parameters:
-##  delta (float): The elapsed time since the last physics frame
-func _physics_process(delta):
+## Called every physics frame to handle character movement and player input.
+## Takes [param delta] for the elapsed time since the last physics frame.
+func _physics_process(delta: float):
 	if target_positions.size() > 0:
 		## Calculate the direction to the current target and move the character
 		var direction = (target_positions[current_target] - global_position).normalized()
