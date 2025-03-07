f09873337813d35317153f1ac43317ce32b212ae
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2025-03-07 11:41:09 -0800

    LevelData documentation finished.

--- scripts/classes/level_data.gd
+++ scripts/classes/level_data.gd
@@ -1,8 +1,20 @@
+## A [Resource] Class for [Level]s.
+##
+## Keeps track of neeeded info for [Level]s. 
 class_name LevelData
+
+# Inherits from [Resource]
 extends Resource
 
+## The id for a [Level]. Set in the editor. 
 @export var level_id : int
+## The name of the [Level]. Set in the editor.
 @export var level_name : String
+## If the [Level] has been completed. Set in the editor. 
 @export var level_completed : bool
+## If the level is unlocked. Set in the editor. 
 @export var level_unlocked : bool
+## The path of the level in the file system. Only needs
+## name before the ".gd" and no folder paths. Set in the 
+## editor. 
 @export var level_path : String
