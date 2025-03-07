58cc0aa437ce9b7780e5d1ccca1f8cbe68d727b9
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2025-03-07 11:33:39 -0800

    Level documentation finished

--- scripts/classes/level.gd
+++ scripts/classes/level.gd
@@ -1,9 +1,19 @@
+## The basic information required for a playable Level.
+##
+## This Script is attached to each level scene. Keeps track of basic info for 
+## each level. 
 class_name Level
+
+# inherits from node
 extends Node
 
+## The unique ID for the level. Set in the editor. 
 @export var level_id : int
+## The start posistion for [Mac]. Defines where [Mac] loads in. 
+## Set in the editor. 
 @export var level_start_position : Node2D
 
+## Extra information is stored in [member LevelData].
 var level_data : LevelData
 func _ready() -> void:
 	level_data = LevelManager.get_level_data_by_id(level_id)
