3ac89f3ae1fe0e15a7434367f21ce3b77d7eb8ee
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2025-03-10 10:48:18 -0700

    TestLevel documentation finished

--- scripts/levels/test_level.gd
+++ scripts/levels/test_level.gd
@@ -1,12 +1,17 @@
+## The testing level for the game. 
+## @deprecated
+
+# Inherits from Node2D. 
 extends Node2D
 
+## The starting position for [Mac]. 
 @export var level_start_pos : Node2D
 
-# Called when the node enters the scene tree for the first time.
+## Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+## Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
